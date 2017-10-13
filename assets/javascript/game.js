eb7cf610769c6c01850cf3a18f09f7c8536941b7
raphatapi <raphaeltapioca10@gmail.com>
2017-10-12 23:42:30 -0600

    style improved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,13 +10,13 @@ var gameLife = 3;
 
 
 function playAgain() {
-	var playBtn = $("<button>");
-    playBtn.addClass("play-button btn btn-success");
-    $("#play").append(playBtn);
-    $(".play-button").on("click", function() {
-        alert("Hello");
-    });
-};
+	// var playBtn = $("<button>");
+ //    playBtn.addClass("play-button btn btn-success");
+ //    $("#play").append(playBtn);
+		$("#play").on("click", function() {
+		    alert("Hello");
+		});
+	};
 
 
 function displayValues() {
@@ -49,20 +49,20 @@ function game(gameScore, gameLife) {
     	counter += crystalValue;
     	$("#userGuess").text(counter);
 
-	    if (counter == targetNumber) {
+	    if (counter === targetNumber) {
 	  	gameScore++;
 	      $("#crystals").remove();
 	      $("h1").text("You win!");
 	      displayValues();
-	      $("#play").append("<br><button onclick='game()'>Play Again!</button>");
+	      // $("#play").append("<br><button onclick='game(gameScore, gameLife)'>Play Again!</button>");
 	    }
 
 	    else if (counter > targetNumber) {
 	    gameLife--;
 	      $("#crystals").remove();
-	      $("h2").text("Better luck next time"); //Game over
+	      $("h2").text("Better luck next time");
 	      displayValues();
-	      $("#play").append("<br><button onclick='game()'>Play Again!</button>");
+	      // $("#play").append("<br><button onclick='game(gameScore, gameLife)'>Play Again!</button>");
 	    }
 	});
 }
@@ -70,4 +70,21 @@ function game(gameScore, gameLife) {
 $(document).ready(function() {
 	game(gameScore, gameLife);
 
+	var audioElement = document.createElement("audio");
+      audioElement.setAttribute("src", "assets/song/theme.mp3");
+      audioElement.play();
+      $("#theme-button").text("Pause Music");
+
+    //Use button to toggle music on/off
+    $("#theme-button").on("click", function() {
+          //If it's not paused then pause it and change button text to "Play Music"
+          if (!audioElement.paused) {
+            audioElement.pause();           
+            $("#theme-button").text("Play Music");
+          //Or do the opposite
+        } else {
+            audioElement.play();           
+          $("#theme-button").text("Pause Music");
+        }
+      });
 });
